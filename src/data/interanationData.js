63f363c906ee2920ffedb20cce3f828f4ac96f1c
ZAKARYA123J <zakaryabaouali255@gmail.com>
2024-08-05 11:49:18 +0200

    ds

--- src/data/interanationData.js
+++ src/data/interanationData.js
@@ -3,6 +3,6 @@ export const Data=[
         image:"",
         name:'Calvin Carlo',
         title:'Manager',
-        desc:'" I was blown away by the personalized support I received from this team. Their expertise in global job placement helped me land a fantastic position abroad. Highly recommend their services! "'
+        file:'" I was blown away by the personalized support I received from this team. Their expertise in global job placement helped me land a fantastic position abroad. Highly recommend their services! "'
     },
 ]
