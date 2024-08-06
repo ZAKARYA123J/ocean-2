082da2a918cbc88f02bcb38a270da6118177005c
AYOUB  BOUNAGA / <99100808+AYOUBBNG@users.noreply.github.com>
2024-08-06 15:45:16 +0100

    formation

--- src/components/BlogDetail.js
+++ src/components/BlogDetail.js
@@ -12,37 +12,32 @@ const BlogDetail = () => {
 
     return (
         <>
-    <div className="flex items-center justify-center min-h-screen">
-            <div className="">
-                <div className="mb-6">
-                    <h1 className="text-4xl font-extrabold text-gray-900 mb-4">{blogPost.title}</h1>
-                    <p className="text-lg text-gray-600 mb-2">{blogPost.date}</p>
+            <div className="flex flex-col lg:flex-row items-center justify-center min-h-screen p-4 lg:p-8">
+                <div className="w-full lg:w-2/3 mb-8 lg:mb-0">
+                    <div className="mb-6">
+                        <h1 className="text-3xl lg:text-4xl font-extrabold text-gray-900 mb-4">{blogPost.title}</h1>
+                        <p className="text-md lg:text-lg text-gray-600 mb-2">{blogPost.date}</p>
+                    </div>
+                    <div className="mb-6">
+                        <img 
+                            src={blogPost.image} 
+                            alt={blogPost.title} 
+                            className="w-full rounded-lg"
+                        />
+                    </div>
+                    <div className="prose prose-gray text-base lg:text-lg">
+                        <p>{blogPost.detail}</p>
+                    </div>
                 </div>
-            <div className="mb-6 ">
-                <img 
-                src={blogPost.image} 
-                alt={blogPost.title} 
-                className=""
-                />
-            </div>
-                <div className="prose prose-gray">
-                     <p>{blogPost.detail}</p>
+                <div className="w-full lg:w-1/3 mb-10 lg:mb-40 p-6 bg-white rounded-lg shadow-lg flex flex-col justify-start items-center">
+                    <p className="text-gray-500 mb-4 lg:mb-6 text-lg lg:text-2xl text-center">To register, simply click here</p>
+                    <button className="bg-purple-600 text-white py-3 lg:py-4 px-10 lg:px-40 rounded-lg text-lg lg:text-xl font-semibold flex items-center">
+                        <span className="mr-2">⚡</span> Réserver
+                    </button>
                 </div>
             </div>
-          <div className=" mb-80 ml-1 p-6 bg-white rounded-lg shadow-lg w-100 h-full flex flex-col justify-start items-start">
-    <div className="w-full flex flex-col items-center">
-        <p className="text-gray-500 mb-6 text-2xl text-center">To register, simply click here</p>
-        <button className="bg-purple-600 text-white py-4 px-40 rounded-lg text-xl font-semibold flex items-center">
-            <span className="mr-2">⚡</span> Réserver
-        </button>
-    </div>
-</div>
-
-
-
-
-            </div>
-            </>
+        </>
     );
 };
+
 export default BlogDetail;
--- src/data/data.js
+++ src/data/data.js
@@ -6,9 +6,8 @@ import client4 from '../assets/images/team/04.jpg'
 import client5 from '../assets/images/team/05.jpg'
 import client6 from '../assets/images/team/06.jpg'
 
-import blog1 from '../assets/images/blog/01.jpg'
+
 import blog2 from '../assets/images/blog/02.jpg'
-import blog3 from '../assets/images/blog/03.jpg'
 import blog4 from '../assets/images/blog/04.jpg'
 import blog5 from '../assets/images/blog/05.jpg'
 import blog6 from '../assets/images/blog/06.jpg'
@@ -21,6 +20,7 @@ import blog12 from '../assets/images/blog/12.jpg'
 import blog13 from '../assets/images/blog/13.jpg'
 import blog14 from '../assets/images/blog/14.jpg'
 import blog15 from '../assets/images/blog/15.jpg'
+import blog16 from '../assets/images/blog/16.jpg'
 
 import company1 from '../assets/images/team/amazon.svg'
 import company2 from '../assets/images/team/google.svg'
@@ -116,7 +116,7 @@ export const blogData = [
         id:1,
         image:blog15,
         title:'Formation de conduitendes bateaux et des voliers',
-        desc: 'This is required when, for example, the final text is not yet available.',
+        desc: 'Cette formation offre une expérience complète pour les passionnés de navigation...',
         detail:"Cette formation offre une expérience complète pour les passionnés de navigation, incluant des cours théoriques et pratiques sur la sécurité, les techniques de voile, et la navigation côtière et hauturière. Les sessions sont dirigées par des instructeurs expérimentés et comprennent des pratiques intensives en mer. Les participants peuvent obtenir des certifications reconnues, leur permettant de piloter divers types de bateaux et de voiliers. Rejoignez-nous pour développer vos compétences nautiques et vivre une aventure maritime inoubliable!",
         type: 'Formation'
     },
@@ -130,9 +130,9 @@ export const blogData = [
     },
     {
         id:3,
-        image:blog3,
+        image:blog16,
         title:'HOME & FAMILY',
-        desc: 'This is required when, for example, the final text is not yet available.',
+        desc: 'La formation Home & Family est conçue pour offrir des compétences pratiques et ...',
         detail:"La formation Home & Family est conçue pour offrir des compétences pratiques et des connaissances approfondies sur la gestion de la vie domestique et des relations familiales. Elle aborde des sujets variés allant de l'organisation du foyer à la gestion des finances familiales, en passant par les techniques de communication et de résolution de conflits. L'objectif est de fournir aux participants les outils nécessaires pour créer un environnement familial harmonieux et bien organisé.",
         type:'Formation'
     },
@@ -140,7 +140,7 @@ export const blogData = [
         id:4,
         image:blog14,
         title:"Formation d'installation et Usilisation VAR",
-        desc:'This is required when, for example, the final text is not yet available.',
+        desc:'La formation en installation et utilisation des VAR (Value Added Resellers)...',
         detail:"La formation en installation et utilisation des VAR (Value Added Resellers) vise à fournir aux professionnels les compétences nécessaires pour installer, configurer et utiliser efficacement les solutions technologiques proposées par les VAR. Elle couvre les étapes d'installation des matériels et logiciels, la configuration avancée, ainsi que l'utilisation quotidienne des outils et applications. Les participants apprennent également à fournir un support technique, diagnostiquer et résoudre les problèmes, effectuer la maintenance préventive, et optimiser les performances des solutions. Cette formation est essentielle pour garantir une gestion efficace des solutions VAR et assurer une performance optimale des systèmes",
         type: 'Formation'
     },
@@ -148,7 +148,7 @@ export const blogData = [
         id:5,
         image:blog12,
         title:'formation a la navigation maritime',
-        desc:'This is required when, for example, the final text is not yet available.',
+        desc:'La formation à la navigation maritime enseigne aux participants les compétences ...',
         detail:"La formation à la navigation maritime enseigne aux participants les compétences essentielles pour naviguer en toute sécurité en mer, en couvrant l'utilisation des outils de navigation comme les cartes maritimes et les systèmes GPS, ainsi que la compréhension des réglementations internationales. Les participants apprennent à planifier des trajets, à gérer les conditions météorologiques, et à assurer la sécurité du navire et de l'équipage. La formation inclut également des techniques de sauvetage et de gestion des situations d'urgence, garantissant une navigation efficace et sécurisée tout en respectant les règles maritimes.",
         type: 'Formation'
     },
@@ -156,7 +156,7 @@ export const blogData = [
         id:6,
         image:blog13,
         title:'Pierres et métaux précieux',
-        desc:'This is required when, for example, the final text is not yet available.',
+        desc:'La formation sur les pierres et métaux précieux fournit une compréhension ...',
         detail:"La formation sur les pierres et métaux précieux fournit une compréhension approfondie des caractéristiques, de l'évaluation et de l'entretien de ces matériaux précieux. Les participants apprennent à identifier et évaluer les pierres précieuses et les métaux précieux, en utilisant des critères de qualité et de certification. La formation couvre également les techniques de traitement et d'entretien pour préserver leur valeur, ainsi que les tendances du marché et les pratiques commerciales. En outre, elle aborde les enjeux éthiques et les pratiques durables liés à l'extraction et au commerce de ces ressources.",
         type: 'Formation'
     },
@@ -164,7 +164,7 @@ export const blogData = [
         id:7,
         image:blog11,
         title:"Développer l'argriculture et la pêche",
-        desc:'This is required when, for example, the final text is not yet available.',
+        desc:"La formation pour développer l'agriculture et la pêche vise à optimiser...",
         detail:"La formation pour développer l'agriculture et la pêche vise à optimiser les pratiques agricoles et halieutiques tout en garantissant la durabilité et la rentabilité. Elle couvre des techniques modernes de culture et de gestion des sols, ainsi que des méthodes de gestion durable des stocks de poissons et des écosystèmes aquatiques. Les participants apprennent à utiliser les dernières technologies et innovations, à adopter des pratiques respectueuses de l'environnement, et à élaborer des stratégies économiques pour améliorer la productivité et accéder aux marchés. Cette formation est essentielle pour les agriculteurs, pêcheurs, et gestionnaires de ressources naturelles souhaitant améliorer leurs pratiques et assurer un développement durable dans leurs secteurs respectifs.",
         type: 'Formation'
     },
@@ -172,7 +172,7 @@ export const blogData = [
         id:8,
         image:blog10,
         title:"Sciences de la terre et Séismes",
-        desc:'This is required when, for example, the final text is not yet available.',
+        desc:'La formation en sciences de la Terre et séismes offre une compréhension ...',
         detail:"La formation en sciences de la Terre et séismes offre une compréhension approfondie des processus géologiques, des mécanismes des séismes, et des techniques d'évaluation des risques sismiques. Elle couvre les principes fondamentaux de la géologie, les mouvements des plaques tectoniques, et les causes des séismes, ainsi que les méthodes de surveillance et de prévision des tremblements de terre. Les participants apprennent également à évaluer les risques sismiques, développer des stratégies pour réduire les risques, et préparer les infrastructures et les communautés aux impacts des séismes. Cette formation est essentielle pour les chercheurs, ingénieurs, et professionnels en gestion des risques souhaitant renforcer leur expertise en sismologie et en réduction des risques.",
         type: 'Formation'
     },
@@ -180,7 +180,7 @@ export const blogData = [
         id:9,
         image:blog2,
         title:"Etudes Formation recrutements national et international",
-        desc:'This is required when, for example, the final text is not yet available.',
+        desc:'La formation en études et recrutement national et international prépare ...',
         detail:"La formation en études et recrutement national et international prépare les participants à gérer les processus de recrutement dans divers contextes géographiques et culturels. Elle couvre les méthodes et techniques de recrutement adaptées aux réalités nationales et internationales, la compréhension des réglementations et des lois du travail, ainsi que l’adaptation des stratégies de recrutement aux différences culturelles. Les participants apprennent également à utiliser les outils modernes de recrutement, tels que les plateformes en ligne et les logiciels de gestion des candidatures. Cette formation est essentielle pour les professionnels des ressources humaines et les recruteurs cherchant à optimiser leurs processus de recrutement et à naviguer efficacement dans des environnements variés",
         type: 'Formation'
     },
@@ -188,7 +188,7 @@ export const blogData = [
         id:10,
         image:blog8,
         title:"Formation DJ",
-        desc:'This is required when, for example, the final text is not yet available.',
+        desc:'La formation DJ enseigne les compétences nécessaires pour devenir un DJ...',
         detail:"La formation DJ enseigne les compétences nécessaires pour devenir un DJ professionnel, en couvrant les techniques de mixage, l'utilisation des équipements modernes comme les platines et les contrôleurs, ainsi que les bases de la production musicale pour créer des remixes et des morceaux originaux. Les participants apprennent également à gérer des événements, à organiser des sets, et à interagir avec le public. En plus des aspects techniques, la formation aide à développer une identité artistique personnelle, à promouvoir son travail en ligne et à construire un portfolio. Elle est idéale pour les débutants et les DJs souhaitant perfectionner leurs compétences et entrer dans le monde de la musique.",
         type: 'Formation'
     },
@@ -197,7 +197,7 @@ export const blogData = [
         id:11,
         image:blog7,
         title:"Apprendre le théatre pour les enfants",
-        desc:'This is required when, for example, the final text is not yet available.',
+        desc:'La formation « Apprendre le théâtre pour les enfants » initie les jeunes...',
         detail:"La formation « Apprendre le théâtre pour les enfants » initie les jeunes à l’art dramatique en développant leur confiance en soi, leurs compétences en communication, et leur créativité. À travers des jeux de rôle, des improvisations, et des exercices de création, les enfants apprennent à exprimer leurs émotions et idées tout en renforçant leur esprit d'équipe et leur capacité à travailler en groupe. La formation introduit également les bases du théâtre, comme la prise de parole en public et la gestion du stress de la scène, offrant ainsi une expérience enrichissante qui stimule à la fois leur développement personnel et leurs compétences sociales",
         type: 'Formation'
     },
@@ -205,7 +205,7 @@ export const blogData = [
         id:12,
         image:blog6,
         title:"Formation ascenseur",
-        desc:'This is required when, for example, the final text is not yet available.',
+        desc:'La formation sur les ascenseurs est conçue pour fournir aux participants...',
         detail:"La formation sur les ascenseurs est conçue pour fournir aux participants une compréhension complète des systèmes d'ascenseurs, de leur installation à leur maintenance. Elle couvre les principes de fonctionnement des ascenseurs, les procédures de sécurité, et les techniques de dépannage. Les participants apprendront à installer, entretenir, et réparer les ascenseurs tout en respectant les normes de sécurité et de réglementation en vigueur. La formation est idéale pour les techniciens, ingénieurs, et toute personne impliquée dans le secteur des ascenseurs, offrant les compétences nécessaires pour assurer le bon fonctionnement et la sécurité des équipements.",
         type: 'Formation'
     },
@@ -213,7 +213,7 @@ export const blogData = [
         id:13,
         image:blog5,
         title:"la conduite de train et de tramway",
-        desc:'This is required when, for example, the final text is not yet available.',
+        desc:'La formation à la conduite de train et de tramway prépare les participants...',
         detail:"La formation à la conduite de train et de tramway prépare les participants à piloter ces véhicules de manière sécurisée et efficace. Elle couvre les aspects techniques de la conduite, y compris le maniement des commandes, la gestion des systèmes de signalisation, et le respect des procédures de sécurité. Les participants apprennent également à gérer les situations d'urgence, à optimiser la consommation d'énergie et à assurer le confort des passagers. La formation inclut des simulations pratiques et théoriques pour développer des compétences approfondies en conduite et en gestion des opérations ferroviaires et de tramway, tout en respectant les normes réglementaires en vigueur.",
         type: 'Formation'
     },
@@ -221,7 +221,7 @@ export const blogData = [
         id:14,
         image:blog4,
         title:'language',
-        desc:'This is required when, for example, the final text is not yet available.',
+        desc:'The Ocean Connecting offers a comprehensive language learning program covering...',
         detail:'The Ocean Connecting offers a comprehensive language learning program covering levels A1 to C2. It features young teachers, engaging activities, educational trips, and flexible schedules. Students benefit from regular level assessments, free access to PCs and Wi-Fi, and air-conditioned classrooms. The program also provides psychological monitoring and guidance, ensuring a supportive learning environment.this program aims to enhance language proficiency through interactive and diverse methods, accommodating different learning paces and needs.',
         type: 'language'
     }
