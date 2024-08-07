9023147a7762c3adba441c17227a0a73d4ec394f
AYOUB  BOUNAGA / <99100808+AYOUBBNG@users.noreply.github.com>
2024-08-07 10:10:26 +0100

    formation

--- src/data/data.js
+++ src/data/data.js
@@ -1,29 +1,30 @@
 import { FiTrendingUp, FiDollarSign, FiCompass,FiCommand, FiBox, FiCamera, FiBell, FiMonitor } from '../assets/icons/vander'
+
+import client6 from '../assets/images/team/06.jpg'
+import { CgWebsite } from "react-icons/cg";
+import { IoHomeOutline } from "react-icons/io5";
+import { MdOutlineCleaningServices } from "react-icons/md";
 import client1 from '../assets/images/team/01.jpg'
 import client2 from '../assets/images/team/02.jpg'
 import client3 from '../assets/images/team/03.jpg'
 import client4 from '../assets/images/team/04.jpg'
 import client5 from '../assets/images/team/05.jpg'
-import client6 from '../assets/images/team/06.jpg'
-import { CgWebsite } from "react-icons/cg";
-import { IoHomeOutline } from "react-icons/io5";
-import { MdOutlineCleaningServices } from "react-icons/md";
-import blog1 from '../assets/images/blog/01.jpg'
-import blog2 from '../assets/images/blog/02.jpg'
-import blog4 from '../assets/images/blog/04.jpg'
-import blog5 from '../assets/images/blog/05.jpg'
-import blog6 from '../assets/images/blog/06.jpg'
-import blog7 from '../assets/images/blog/07.jpg'
-import blog8 from '../assets/images/blog/08.jpg'
-import blog9 from '../assets/images/blog/09.jpg'
-import blog10 from '../assets/images/blog/10.jpg'
-import blog11 from '../assets/images/blog/11.jpg'
-import blog12 from '../assets/images/blog/12.jpg'
-import blog13 from '../assets/images/blog/13.jpg'
-import blog14 from '../assets/images/blog/14.jpg'
-import blog15 from '../assets/images/blog/15.jpg'
-import blog16 from '../assets/images/blog/16.jpg'
-import blog3 from '../assets/images/blog/03.jpg'
+import formation4 from '../assets/images/formation/formation4.jpg'
+import formation5 from '../assets/images/formation/formation05.jpg'
+import formation6 from '../assets/images/formation/formation6.jpg'
+import formation17 from '../assets/images/formation/formation17.jpg'
+import formation8 from '../assets/images/formation/formation8.jpg'
+import formation9 from '../assets/images/formation/formation9.jpg'
+import formation10 from '../assets/images/formation/formation10.jpg'
+import formation11 from '../assets/images/formation/formation11.jpg'
+import formation12 from '../assets/images/formation/formation12.jpg'
+import formation13 from '../assets/images/formation/formation13.jpg'
+import formation14 from '../assets/images/formation/formation14.jpg'
+import formation15 from '../assets/images/formation/15.jpg'
+import formation16 from '../assets/images/formation/16.jpg'
+import formation18 from '../assets/images/formation/formation18.jpg'
+import formation19 from '../assets/images/formation/formation19.jpg'
+
 import service1 from '../assets/images/service/service1.png'
 import service2 from '../assets/images/service/service2.png'
 import service3 from '../assets/images/service/service3.png'
@@ -95,7 +96,7 @@ export const clientData = [
 export const blogData = [
     {
         id:1,
-        image:blog15,
+        image:formation15,
         title:'Formation de conduitendes bateaux et des voliers',
         desc: 'Cette formation offre une expérience complète pour les passionnés de navigation...',
         detail:"Cette formation offre une expérience complète pour les passionnés de navigation, incluant des cours théoriques et pratiques sur la sécurité, les techniques de voile, et la navigation côtière et hauturière. Les sessions sont dirigées par des instructeurs expérimentés et comprennent des pratiques intensives en mer. Les participants peuvent obtenir des certifications reconnues, leur permettant de piloter divers types de bateaux et de voiliers. Rejoignez-nous pour développer vos compétences nautiques et vivre une aventure maritime inoubliable!",
@@ -103,7 +104,7 @@ export const blogData = [
     },
     {
         id:2,
-        image:blog9,
+        image:formation9,
         title:'Formation Agent de Checkin Aeroport',
         desc:'La formation pour un Agent de Check-in Aéroport prépare les professionnels ...',
         detail:"La formation pour un Agent de Check-in Aéroport prépare les professionnels à gérer efficacement l'enregistrement des passagers et à assurer le bon déroulement des opérations aéroportuaires. Elle couvre l'accueil des passagers, la vérification des documents et la gestion des bagages, tout en utilisant des systèmes informatiques de réservation et de contrôle des bagages. Les participants apprennent également les procédures de sécurité aéroportuaire, les réglementations internationales, et les techniques de service client pour offrir une expérience agréable. La formation comprend des simulations pratiques pour appliquer les connaissances acquises et développer des compétences en communication et en gestion des situations d'urgence.",
@@ -111,7 +112,7 @@ export const blogData = [
     },
     {
         id:3,
-        image:blog16,
+        image:formation16,
         title:'HOME & FAMILY',
         desc: 'La formation Home & Family est conçue pour offrir des compétences pratiques et ...',
         detail:"La formation Home & Family est conçue pour offrir des compétences pratiques et des connaissances approfondies sur la gestion de la vie domestique et des relations familiales. Elle aborde des sujets variés allant de l'organisation du foyer à la gestion des finances familiales, en passant par les techniques de communication et de résolution de conflits. L'objectif est de fournir aux participants les outils nécessaires pour créer un environnement familial harmonieux et bien organisé.",
@@ -119,7 +120,7 @@ export const blogData = [
     },
     {
         id:4,
-        image:blog14,
+        image:formation14,
         title:"Formation d'installation et Usilisation VAR",
         desc:'La formation en installation et utilisation des VAR (Value Added Resellers)...',
         detail:"La formation en installation et utilisation des VAR (Value Added Resellers) vise à fournir aux professionnels les compétences nécessaires pour installer, configurer et utiliser efficacement les solutions technologiques proposées par les VAR. Elle couvre les étapes d'installation des matériels et logiciels, la configuration avancée, ainsi que l'utilisation quotidienne des outils et applications. Les participants apprennent également à fournir un support technique, diagnostiquer et résoudre les problèmes, effectuer la maintenance préventive, et optimiser les performances des solutions. Cette formation est essentielle pour garantir une gestion efficace des solutions VAR et assurer une performance optimale des systèmes",
@@ -127,7 +128,7 @@ export const blogData = [
     },
     {
         id:5,
-        image:blog12,
+        image:formation12,
         title:'formation a la navigation maritime',
         desc:'La formation à la navigation maritime enseigne aux participants les compétences ...',
         detail:"La formation à la navigation maritime enseigne aux participants les compétences essentielles pour naviguer en toute sécurité en mer, en couvrant l'utilisation des outils de navigation comme les cartes maritimes et les systèmes GPS, ainsi que la compréhension des réglementations internationales. Les participants apprennent à planifier des trajets, à gérer les conditions météorologiques, et à assurer la sécurité du navire et de l'équipage. La formation inclut également des techniques de sauvetage et de gestion des situations d'urgence, garantissant une navigation efficace et sécurisée tout en respectant les règles maritimes.",
@@ -135,7 +136,7 @@ export const blogData = [
     },
     {
         id:6,
-        image:blog13,
+        image:formation13,
         title:'Pierres et métaux précieux',
         desc:'La formation sur les pierres et métaux précieux fournit une compréhension ...',
         detail:"La formation sur les pierres et métaux précieux fournit une compréhension approfondie des caractéristiques, de l'évaluation et de l'entretien de ces matériaux précieux. Les participants apprennent à identifier et évaluer les pierres précieuses et les métaux précieux, en utilisant des critères de qualité et de certification. La formation couvre également les techniques de traitement et d'entretien pour préserver leur valeur, ainsi que les tendances du marché et les pratiques commerciales. En outre, elle aborde les enjeux éthiques et les pratiques durables liés à l'extraction et au commerce de ces ressources.",
@@ -143,7 +144,7 @@ export const blogData = [
     },
     {
         id:7,
-        image:blog11,
+        image:formation11,
         title:"Développer l'argriculture et la pêche",
         desc:"La formation pour développer l'agriculture et la pêche vise à optimiser...",
         detail:"La formation pour développer l'agriculture et la pêche vise à optimiser les pratiques agricoles et halieutiques tout en garantissant la durabilité et la rentabilité. Elle couvre des techniques modernes de culture et de gestion des sols, ainsi que des méthodes de gestion durable des stocks de poissons et des écosystèmes aquatiques. Les participants apprennent à utiliser les dernières technologies et innovations, à adopter des pratiques respectueuses de l'environnement, et à élaborer des stratégies économiques pour améliorer la productivité et accéder aux marchés. Cette formation est essentielle pour les agriculteurs, pêcheurs, et gestionnaires de ressources naturelles souhaitant améliorer leurs pratiques et assurer un développement durable dans leurs secteurs respectifs.",
@@ -151,21 +152,23 @@ export const blogData = [
     },
     {
         id:8,
-        image:blog10,
+        image:formation10,
         title:"Sciences de la terre et Séismes",
         desc:'La formation en sciences de la Terre et séismes offre une compréhension ...',
         detail:"La formation en sciences de la Terre et séismes offre une compréhension approfondie des processus géologiques, des mécanismes des séismes, et des techniques d'évaluation des risques sismiques. Elle couvre les principes fondamentaux de la géologie, les mouvements des plaques tectoniques, et les causes des séismes, ainsi que les méthodes de surveillance et de prévision des tremblements de terre. Les participants apprennent également à évaluer les risques sismiques, développer des stratégies pour réduire les risques, et préparer les infrastructures et les communautés aux impacts des séismes. Cette formation est essentielle pour les chercheurs, ingénieurs, et professionnels en gestion des risques souhaitant renforcer leur expertise en sismologie et en réduction des risques.",
         type: 'Formation'
     },
     {
-        image:blog8,
-        title:'Arbitration Techniques Installation and Use',
-        desc:'We provide expert installation and guidance on arbitration techniques, ensuring efficient setup and effective use for resolving disputes.',
-        date:'5th May 2023'
+        id:9,
+        image:formation19,
+        title:"Etudes Formation Recrutements National Et International",
+        desc:'Études Formation Recrutements National et International (EFRNI) est ...',
+        detail:"Études Formation Recrutements National et International (EFRNI) est une formation spécialisée qui vise à développer des compétences avancées dans le domaine du recrutement, tant au niveau national qu'international. Elle couvre les enjeux liés au recrutement dans divers contextes culturels et légaux, tout en enseignant l'utilisation des outils modernes pour identifier et attirer les talents. La formation met également l'accent sur l'optimisation des processus de sélection et sur l'importance de la gestion des talents pour assurer la rétention et le développement des employés dans une perspective globale",
+        type: 'Formation'
     },
     {
         id:10,
-        image:blog8,
+        image:formation8,
         title:"Formation DJ",
         desc:'La formation DJ enseigne les compétences nécessaires pour devenir un DJ...',
         detail:"La formation DJ enseigne les compétences nécessaires pour devenir un DJ professionnel, en couvrant les techniques de mixage, l'utilisation des équipements modernes comme les platines et les contrôleurs, ainsi que les bases de la production musicale pour créer des remixes et des morceaux originaux. Les participants apprennent également à gérer des événements, à organiser des sets, et à interagir avec le public. En plus des aspects techniques, la formation aide à développer une identité artistique personnelle, à promouvoir son travail en ligne et à construire un portfolio. Elle est idéale pour les débutants et les DJs souhaitant perfectionner leurs compétences et entrer dans le monde de la musique.",
@@ -174,7 +177,7 @@ export const blogData = [
     
     {
         id:11,
-        image:blog7,
+        image:formation17,
         title:"Apprendre le théatre pour les enfants",
         desc:'La formation « Apprendre le théâtre pour les enfants » initie les jeunes...',
         detail:"La formation « Apprendre le théâtre pour les enfants » initie les jeunes à l’art dramatique en développant leur confiance en soi, leurs compétences en communication, et leur créativité. À travers des jeux de rôle, des improvisations, et des exercices de création, les enfants apprennent à exprimer leurs émotions et idées tout en renforçant leur esprit d'équipe et leur capacité à travailler en groupe. La formation introduit également les bases du théâtre, comme la prise de parole en public et la gestion du stress de la scène, offrant ainsi une expérience enrichissante qui stimule à la fois leur développement personnel et leurs compétences sociales",
@@ -182,7 +185,7 @@ export const blogData = [
     },
     {
         id:12,
-        image:blog6,
+        image:formation18,
         title:"Formation ascenseur",
         desc:'La formation sur les ascenseurs est conçue pour fournir aux participants...',
         detail:"La formation sur les ascenseurs est conçue pour fournir aux participants une compréhension complète des systèmes d'ascenseurs, de leur installation à leur maintenance. Elle couvre les principes de fonctionnement des ascenseurs, les procédures de sécurité, et les techniques de dépannage. Les participants apprendront à installer, entretenir, et réparer les ascenseurs tout en respectant les normes de sécurité et de réglementation en vigueur. La formation est idéale pour les techniciens, ingénieurs, et toute personne impliquée dans le secteur des ascenseurs, offrant les compétences nécessaires pour assurer le bon fonctionnement et la sécurité des équipements.",
@@ -190,7 +193,7 @@ export const blogData = [
     },
     {
         id:13,
-        image:blog5,
+        image:formation5,
         title:"la conduite de train et de tramway",
         desc:'La formation à la conduite de train et de tramway prépare les participants...',
         detail:"La formation à la conduite de train et de tramway prépare les participants à piloter ces véhicules de manière sécurisée et efficace. Elle couvre les aspects techniques de la conduite, y compris le maniement des commandes, la gestion des systèmes de signalisation, et le respect des procédures de sécurité. Les participants apprennent également à gérer les situations d'urgence, à optimiser la consommation d'énergie et à assurer le confort des passagers. La formation inclut des simulations pratiques et théoriques pour développer des compétences approfondies en conduite et en gestion des opérations ferroviaires et de tramway, tout en respectant les normes réglementaires en vigueur.",
@@ -198,7 +201,7 @@ export const blogData = [
     },
     {
         id:14,
-        image:blog4,
+        image:formation6,
         title:'language',
         desc:'The Ocean Connecting offers a comprehensive language learning program covering...',
         detail:'The Ocean Connecting offers a comprehensive language learning program covering levels A1 to C2. It features young teachers, engaging activities, educational trips, and flexible schedules. Students benefit from regular level assessments, free access to PCs and Wi-Fi, and air-conditioned classrooms. The program also provides psychological monitoring and guidance, ensuring a supportive learning environment.this program aims to enhance language proficiency through interactive and diverse methods, accommodating different learning paces and needs.',
--- src/pages/formation.js
+++ src/pages/formation.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import hero1 from '../assets/images/home.png'
 import Navbar from "../components/navbar";
 import Formation from "../components/formation";
+import Footer from "../components/footer";
 
 
 export default function IndexSix(){
@@ -14,6 +15,7 @@ export default function IndexSix(){
             <section >
                  <Formation/>
             </section>
+            <Footer/>
         </>
     )
 }
