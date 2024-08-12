fc4b26c4137ecccfbbf56afec97d44fe30f95a4b
akajdid-mostafa <mostafaakajdid6@gmail.com>
2024-08-12 10:59:32 +0100

    mos

--- src/components/recruitment/Jobs.js
+++ src/components/recruitment/Jobs.js
@@ -94,7 +94,7 @@ const JobListings = () => {
                  <p className="text-blue-600 font-semibold text-sm">{job.type}</p>
                  {/* if  (job.id === 1 ) { */}
                  <Link to={`${job.link}`} className="inline-flex items-center justify-center px-3 py-2 bg-blue-500 text-white text-sm font-semibold rounded-md shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300">
-                   <span>Demande</span>
+                   <span>{job.action}</span>
                            <FaAngleRight className="ml-2" />
                   </Link>
                   {/* // } 
