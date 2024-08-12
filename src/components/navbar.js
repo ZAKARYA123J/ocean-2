7465043b9946e69ba6968ceb74988f550b65dc91
ZAKARYA123J <zakaryabaouali255@gmail.com>
2024-08-12 17:24:21 +0200

    ez

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -61,11 +61,12 @@ export default function Navbar(){
                             <Link to="" className="h-8 px-4 text-[12px] tracking-wider inline-flex items-center justify-center font-medium rounded-md bg-blue-500 text-white uppercase">Login</Link>
                         </li>
                     </ul> */}
+                     <button className="nav-link lg:hidden"><LanguageSwitcher /></button>
                     <button data-collapse="menu-collapse" type="button" className="collapse-btn inline-flex items-center ms-2 text-dark dark:text-white lg_992:hidden" onClick={() =>setManu(!manu)}>
                         <span className="sr-only">Navigation Menu</span>
                         <i className="mdi mdi-menu text-[24px]"></i>
                     </button>
-                    <button className="nav-link lg:hidden"><LanguageSwitcher /></button>
+                   
                 </div>
 
                 <div className={`navigation lg_992:order-1 lg_992:flex  ms-auto ${manu ? '' : 'hidden'}`} id="menu-collapse">
