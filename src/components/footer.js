97c48c45071834e51d9e248b8065fcf232179d11
ZAKARYA123J <zakaryabaouali255@gmail.com>
2024-08-15 13:52:26 +0200

    ere

--- src/components/footer.js
+++ src/components/footer.js
@@ -15,13 +15,11 @@ export default function Footer(){
                                 <img src={img} className="mx-auto md:me-auto md:ms-0" alt="" style={{width:"100px"}}/>
                             </Link>
                         </div>
-
                         <div className="lg:col-span-5 text-center mt-6 md:mt-0">
                             <p className="mb-0">© {new Date().getFullYear()} Ocean Connect</p>
                             <p className="mb-0">oceanconnecting.ma@gmail.com</p>
                             <p>+212 704-309787</p>
                         </div>
-
                         <ul className="lg:col-span-4 list-none md:text-end text-center mt-6 md:mt-0">
                             <li className="inline"><Link to="https://www.linkedin.com/company/ocean-connecting/?originalSubdomain=ma" target="_blank" className="size-8 inline-flex justify-center items-center border border-gray-800 rounded-md hover:border-sky-500 dark:hover:border-sky-500 hover:bg-sky-500 dark:hover:bg-sky-500"><FiLinkedin className="size-4 align-middle" title="Linkedin"/></Link></li>
                             <li className="inline"><Link to="https://www.facebook.com/the.ocean.connecting/" target="_blank" className="size-8 inline-flex justify-center items-center border border-gray-800 rounded-md hover:border-sky-500 dark:hover:border-sky-500 hover:bg-sky-500 dark:hover:bg-sky-500"><FiFacebook className="size-4 align-middle" title="instagram"/></Link></li>
